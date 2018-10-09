8244e9113cea88de1be909abb8ac4f918f60c1a2
MagdaBM <buczek.magdalena@gmail.com>
2018-10-09 22:01:42 +0200

    PESEL file corrected

--- homework01/pesel.rb
+++ homework01/pesel.rb
@@ -1,20 +1,23 @@
-puts "Wprowadź pesel"
-pesel = gets
+puts "Wprowadź PESEL:"
+pesel = gets.chomp
 
 a = pesel.chars   # a zamiast array
 
-control = 9*a[0]+7*a[1]+3*a[2]+1*a[3]+9*a[4]+7*a[5]+3*a[6]+1*a[7]+9*a[8]+7*a[9]
-
-while
-
-
+control = 9 * (a[0].to_i + a[4].to_i + a[8].to_i) +
+          7 * (a[1].to_i + a[5].to_i + a[9].to_i) +
+          3 * (a[2].to_i + a[6].to_i) + a[3].to_i + a[7].to_i
 
 puts "Informacje o PESELu:"
 
-if array[9].to_i % 2 == 0
-  puts "Płeć: kobieta"
+if a.count == 11  &&  control % 10 == a[10].to_i
+  puts "PESEL jest poprawny."
+  if a[9].to_i % 2 == 0
+    puts "Płeć: kobieta."
+  else
+    puts "Płeć: mężczyzna."
+  end
 else
-  puts "Płeć: mężczyzna"
+  puts "PESEL jest niepoprawny."
 end
 
-puts "Data urodzenia: #{array[4].to_i}#{array[5].to_i}-#{array[2].to_i}#{array[3].to_i}-19#{array[0].to_i}#{array[1].to_i}"
+puts "Data urodzenia: #{a[4, 2].join}-#{a[2, 2].join}-19#{a[0, 2].join}"
