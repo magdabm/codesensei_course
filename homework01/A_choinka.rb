8a4eaca27fdba006d1f39370d5c705a204e923b5
MagdaBM <buczek.magdalena@gmail.com>
2018-10-17 23:16:28 +0200

    choinka file

---
+++ homework01/A_choinka.rb
@@ -0,0 +1,20 @@
+
+p = 4
+c = 1
+
+while c <= 7
+  puts " " * p + "*" * c + " " * p
+  p = p - 1
+  c = c + 2
+end
+
+p = 3
+c = 3
+
+while c <= 9
+  puts " " * p + "*" * c + " " * p
+  p = p - 1
+  c = c + 2
+end
+
+puts " " * 4 + "*" * 1 + " " * 4
