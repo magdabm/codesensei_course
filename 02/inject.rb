ce4c02726994e465609cc2b45ddc3d082130a767
MagdaBM <buczek.magdalena@gmail.com>
2018-10-23 00:30:56 +0200

    change files structure

--- 02/inject.rb
+++
@@ -1,5 +0,0 @@
-["a", "bb", "ccc"].inject(0) do |accumulator, element|
-  accumulator + element.length
-end
-
-# accumulator - zmienna do przechowywania wartości w każdym kroku, w tym przypadku sumy, która zwiększa się po dodaniu każdego kolejnego elementu
