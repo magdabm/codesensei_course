a25bd721f5fb83d6ab62aee39bd6f9daaf7fd3be
MagdaBM <buczek.magdalena@gmail.com>
2018-10-27 00:03:19 +0200

    first homework file

---
+++ 02_homework/01_initials.rb
@@ -0,0 +1,18 @@
+# Napisz metodę initials , która wypisuje inicjały osoby na podstawie jej pełnego imienia. Wszystkie znaki inicjałów powinny być pisane z dużej litery. Np:
+# initials('Jan Kozlowski') #=> 'JK'
+# initials('Jan Krzysztof Rakoczy') #=> 'JKR'
+# initials('marcin nowak') #=> 'MN'
+
+def initials(string)
+  name = string.chomp.split
+  init = ""
+  name.each do |elem|
+    init += elem[0]
+  end
+  init.upcase
+end
+
+puts initials("Jan Nowak Jeziorański")
+puts initials("szach mat")
+puts initials("Morus")
+puts initials("Imię imię nazwisko Nazwisko")
