dfcbf38e4a3328c4276d0c5dfa6987ec4c86dfbf
MagdaBM <buczek.magdalena@gmail.com>
2018-10-30 01:13:27 +0100

    array average file - no correct solution

--- 02_homework/09_array_average_NO_SOLUTION.rb
+++ 02_homework/09_array_average_NO_SOLUTION.rb
@@ -3,13 +3,13 @@
 # avg_array([1, 5, 3, 22], [12, 22, 13, 5], [5, 12, 24, 5], [14, 40, 5, 17]) #=> [8, 19.75, 11.25, 13.5]
 
 
-# dla dwóch tablic, nie działa
+# dla dwóch tablic, nie działa (tzn. ostatni elemnt w wynikowej tablicy sie dubluje....)
 
 def avg_array(a1, a2)
   a_length = a1.length
   avg_a = []
   i = 0
-  while i < (a_length-1)
+  while i < (a_length)
     avg_a[i] = (a1[i] + a2[i])/2
     avg_a.push(avg_a[i])
     i += 1
