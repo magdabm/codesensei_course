000831febae157e5be7dccfe2741472a5352c960
MagdaBM <buczek.magdalena@gmail.com>
2018-11-15 01:28:10 +0100

    products files - finding products with lower and bigger price

---
+++ 03_homework/05_products/05B_csv_require_ProductsList.rb
@@ -0,0 +1,45 @@
+class ProductsList
+  attr_writer :products
+
+  def initialize
+    self.products = []
+  end
+
+  def parse
+    self.products = csv_data.map do |elements|
+      Product.new(elements[0], elements[1].to_f, elements[2].to_f)
+    end
+  end
+
+  def products
+    if @products.count == 0
+      parse
+    else
+      @products
+    end
+  end
+
+  def find_product(name)
+    products.find { |product| product.name == name }
+  end
+
+  def find_lower_price(price)
+    products.find_all { |product| product.price < price }
+  end
+
+  def find_bigger_price(price)
+    products.find_all { |product| product.price > price }
+  end
+
+  private
+
+  def file
+    @file ||= File.read("products.csv")
+  end
+
+  def csv_data
+    csv_array = CSV.parse(file)
+    csv_array.shift
+    csv_array
+  end
+end
---
+++ 03_homework/05_products/05_csv_products_ARGV.rb
@@ -0,0 +1,32 @@
+require "csv"   # ten zapis wczytuje do tego pliku możliwość używania rozszerzenia csv / metod na csv (?)
+require "./19A_CSV_product_require.rb"   # ten zapis jest odwołaniem do innego pliku, informuje że do wykonania kodu w tym pliku potrzebne są dane zawarte w powyższym pliku
+require "./19B_CSV_products_list_require.rb"
+
+products_list = ProductsList.new
+
+if ARGV[0] == "-f"
+  product = products_list.find_product(ARGV[1])
+  puts "#{product.name}: #{product.price} zł"
+else
+  puts "USAGE: ruby products.rb FUNCTION ARGUMENT"
+end
+
+if ARGV[0] == "-gt"
+  products = products_list.find_bigger_price(ARGV[1].to_f)
+  puts "Products with bigger price than '#{ARGV[1]}':"
+  products.each do |product|
+     puts "#{product.name}: #{product.price} zł"
+  end
+else
+  puts "USAGE: ruby products.rb FUNCTION ARGUMENT"
+end
+
+if ARGV[0] == "-lt"
+  products = products_list.find_lower_price(ARGV[1].to_f)
+  puts "Products with lower price than '#{ARGV[1]}':"
+  products.each do |product|
+     puts "#{product.name}: #{product.price} zł"
+  end
+else
+  puts "USAGE: ruby products.rb FUNCTION ARGUMENT"
+end
